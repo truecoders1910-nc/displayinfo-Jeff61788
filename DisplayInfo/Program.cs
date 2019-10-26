87ef0bae4201c44d38c587daa34bd5fd927aa5e3
Jeff Aycock <Jaycock@Jeffs-MacBook-Pro.local>
2019-10-26 10:15:15 -0500

    first four steps complete

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -15,6 +15,26 @@ namespace DisplayInfo
 
             // If their favorite number is greater than 10, display it in green.
             // If their favorite number is less than 5, display it in red.
+
+
+            Console.WriteLine("What is Your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName}");
+
+            Console.WriteLine("What Is Your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Nice To Meet You {firstName} {lastName}");
+
+            Console.WriteLine("What Is Your Favorite Number?");
+            string favNum1 = Console.ReadLine();
+            Console.WriteLine($"Your Favorite Number is {favNum1}");
+
+            Console.WriteLine("What Is Your Favorite Animal");
+            string animal = Console.ReadLine();
+            Console.WriteLine($"Your Favorite Animal is {animal}");
+            
+           
         }
+            
     }
 }
